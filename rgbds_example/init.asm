3af97e92136b9be3e265c23d92d94c9f70ad76d1
AntonioND <antonio_nd@outlook.com>
2015-04-27 01:48:24 +0200

    Changed tabs by spaces in asm code.
    Changed tabs by spaces in asm code.

--- rgbds_example/init.asm
+++ rgbds_example/init.asm
@@ -1,62 +1,60 @@
 
-	INCLUDE	"hardware.inc"
-	INCLUDE	"gbt_player.inc"
-	
-	GLOBAL	song_data
+    INCLUDE "hardware.inc"
+    INCLUDE "gbt_player.inc"
+
+    GLOBAL  song_data
 
 ;--------------------------------------------------------------------------
 ;-                             CARTRIDGE HEADER                           -
 ;--------------------------------------------------------------------------
-	
-	SECTION	"Cartridge Header",ROM0[$0100]
-	
-	nop
-	jp	StartPoint
 
-	NINTENDO_LOGO
-	
-		;0123456789ABC
-	DB	"GBT Tester"
-	DW	$0000
-	DB	$00     ;GBC flag
-	DB	0,0,0	;SuperGameboy
-	DB	CART_ROM_MBC5 ;CARTTYPE (MBC5)
-	DB	0	;ROMSIZE
-	DB	0	;RAMSIZE
+    SECTION "Cartridge Header",ROM0[$0100]
 
-	DB	$01     ;Destination (0 = Japan, 1 = Non Japan)
-	DB	$00	;Manufacturer
+    nop
+    jp  StartPoint
 
-	DB	0	;Version
-	DB	0	;Complement check
-	DW	0	;Checksum
+    NINTENDO_LOGO
+
+    ;    0123456789ABCDE
+    DB  "GBT TESTER     "
+    DB  $00 ;GBC flag
+    DB  0,0,0 ;SGB
+    DB  CART_ROM_MBC5 ;CARTTYPE (MBC5)
+    DB  0   ;ROMSIZE
+    DB  0   ;RAMSIZE
+
+    DB  $01 ;Destination (0 = Japan, 1 = Non Japan)
+    DB  $00 ;Manufacturer
+
+    DB  0   ;Version
+    DB  0   ;Complement check
+    DW  0   ;Checksum
 
 ;--------------------------------------------------------------------------
 ;-                          INITIALIZE THE GAMEBOY                        -
 ;--------------------------------------------------------------------------
 
-	SECTION	"Program Start",ROM0[$0150]
-	
+    SECTION "Program Start",ROM0[$0150]
+
 StartPoint:
 
-	di
-	
-	ld	sp,$D000 ; Set stack
-	
-	ld	a,$01
-	ld	[rIE],a ; Enable VBL interrupt
-	
-	ld	de,song_data
-	ld	bc,BANK(song_data)
-	ld	a,$05
-	call	gbt_play ; Play song
-	
-.loop:
-	halt
-	xor	a,a
-	ld	[rIF],a
-	
-	call	gbt_update ; Update player
-	jr	.loop
+    di
 
-	
+    ld  sp,$D000 ; Set stack
+
+    ld  a,$01
+    ld  [rIE],a ; Enable VBL interrupt
+
+    ld  de,song_data
+    ld  bc,BANK(song_data)
+    ld  a,$05
+    call gbt_play ; Play song
+
+.loop:
+    halt
+    xor a,a
+    ld  [rIF],a
+
+    call gbt_update ; Update player
+    jr  .loop
+
