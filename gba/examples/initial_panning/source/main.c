28efce4b5335aada23a7532d1645d966f2767449
Antonio Niño Díaz <antonio_nd@outlook.com>
2022-05-15 22:55:17 +0100

    gba: examples: Add example of setting initial panning

---
+++ gba/examples/initial_panning/source/main.c
@@ -0,0 +1,26 @@
+// GBT Player v4.2.0
+//
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2022 Antonio Niño Díaz <antonio_nd@outlook.com>
+
+// This example uses a S3M file that has preset panning values for each channel,
+// and modifies them during the playback of the song.
+
+#include <gba.h>
+
+#include "gbt_player/gbt_player.h"
+
+extern const uint8_t *template[];
+
+int main(int argc, char *argv[])
+{
+    irqInit();
+    irqSet(IRQ_VBLANK, gbt_update);
+    irqEnable(IRQ_VBLANK);
+
+    gbt_play(template, -1);
+
+    while (1)
+        VBlankIntrWait();
+}
