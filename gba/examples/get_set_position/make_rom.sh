191ca896d8807ca9aa31703c6dd16810ae92ce5b
Antonio Niño Díaz <antonio_nd@outlook.com>
2022-05-15 14:53:17 +0100

    gba: example: Add example to get and set position of a song

---
+++ gba/examples/get_set_position/make_rom.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir -p music
+
+# MOD files:
+#../mod2gbt/mod2gbt template.mod template
+#mv template.c music
+
+# S3M files:
+../../s3m2gbt/s3m2gbt.py \
+    --input template.s3m \
+    --name template \
+    --output music/template.c \
+    --instruments
+
+make
---
+++ gba/examples/get_set_position/source/main.c
@@ -0,0 +1,58 @@
+// GBT Player v4.2.0
+//
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2022 Antonio Niño Díaz <antonio_nd@outlook.com>
+
+// This example shows how to set and get the position of the current song.
+
+#include <stdio.h>
+
+#include <gba.h>
+
+#include "gbt_player/gbt_player.h"
+
+extern const uint8_t *template[];
+
+int main(int argc, char *argv[])
+{
+    irqInit();
+    irqEnable(IRQ_VBLANK);
+
+    consoleDemoInit();
+
+    iprintf("\n");
+    iprintf("\n");
+    iprintf("A: Jump to order 2, row 32\n");
+    iprintf("B: Jump to order 1, row 0\n");
+    iprintf("START: Jump to start\n");
+
+    gbt_play(template, -1);
+
+    while (1)
+    {
+        VBlankIntrWait();
+        gbt_update();
+
+        scanKeys();
+        int keys = keysDown();
+
+        if (keys & KEY_A)
+        {
+            gbt_set_position(2, 32);
+        }
+        if (keys & KEY_B)
+        {
+            gbt_set_position(1, 0);
+        }
+        if (keys & KEY_START)
+        {
+            gbt_set_position(0, 0);
+        }
+
+        int order, row, tick;
+        gbt_get_position(&order, &row, &tick);
+        iprintf("\x1b[0;0HOrder %d | Row %2d | Tick %d", order, row, tick);
+    }
+}
+
